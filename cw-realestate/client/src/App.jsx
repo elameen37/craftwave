e0c8292740ae783e2d797e3d1c9061e621c89689
elameen37 <elameen37@gmail.com>
2023-11-06 13:15:04 +0100

    update header and make profile private

--- cw-realestate/client/src/App.jsx
+++ cw-realestate/client/src/App.jsx
@@ -5,17 +5,22 @@ import SignUp from "./pages/SignUp";
 import About from "./pages/About";
 import SignIn from "./pages/Signin";
 import Header from "./components/Header";
-
+import PrivateRoute from "./components/PrivateRoute";
 
 export default function App() {
-  return <BrowserRouter>
-  <Header />
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/profile" element={<Profile />} />
-      <Route path="/sign-in" element={<SignIn />} />
-      <Route path="/sign-up" element={<SignUp />} />
-      <Route path="/about" element={<About />} />
-    </Routes>
-  </BrowserRouter>
+  return (
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+
+        <Route element={<PrivateRoute />}>
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
--- cw-realestate/client/src/components/Header.jsx
+++ cw-realestate/client/src/components/Header.jsx
@@ -1,7 +1,27 @@
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search);
+    urlParams.set("searchTerm", searchTerm);
+    const searchQuery = urlParams.toString();
+    navigate(`/search?${searchQuery}`);
+  };
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
+    }
+  }, [location.search]);
   return (
     <header className="bg-indigo-200 shadow-sm">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-4">
@@ -11,13 +31,20 @@ export default function Header() {
             <span className="text-indigo-950">RealEstate</span>
           </h1>
         </Link>
-        <form className="bg-indigo-100 p-2 rounded-full flex items-center">
+        <form
+          onSubmit={handleSubmit}
+          className="bg-indigo-100 p-2 rounded-full flex items-center"
+        >
           <input
             type="text"
             placeholder="Search..."
             className="bg-transparent focus:outline-none w-28 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
           />
-          <FaSearch className="text-indigo-950" />
+          <button>
+            <FaSearch className="text-indigo-950" />
+          </button>
         </form>
         <ul>
           <Link to="/">
@@ -30,10 +57,18 @@ export default function Header() {
               About
             </li>
           </Link>
-          <Link to="/sign-in">
-            <li className="sm:inline-block mx-2 sm:mx-4 text-sm sm:text-lg text-indigo-950 hover:opacity-75">
-              Sign In
-            </li>
+          <Link to="/profile">
+            {currentUser ? (
+              <img
+                className="rounded-full h-10 w-10 object-cover"
+                src={currentUser.avatar}
+                alt="profile"
+              />
+            ) : (
+              <li className="sm:inline-block mx-2 sm:mx-4 text-sm sm:text-lg text-indigo-950 hover:opacity-75">
+                Sign In
+              </li>
+            )}
           </Link>
         </ul>
       </div>
