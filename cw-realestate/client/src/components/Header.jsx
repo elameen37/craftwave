ed44f0df986222d43311758f895f7d2d7e705d0d
elameen37 <elameen37@gmail.com>
2023-11-23 17:37:45 +0100

    complete header search functionality

--- cw-realestate/client/src/components/Header.jsx
+++ cw-realestate/client/src/components/Header.jsx
@@ -60,7 +60,7 @@ export default function Header() {
           <Link to='/profile'>
             {currentUser ? (
               <img
-                className='rounded-full h-8 w-8 object-cover'
+                className='rounded-full h-7 w-7 object-cover'
                 src={currentUser.avatar}
                 alt='profile'
               />
